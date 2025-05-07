788d474dffaf46efda2a625d804369d163c13da0
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-05-07 17:34:33 +0400

    fix

--- extentions/CodeFormer/codeformer.py
+++ extentions/CodeFormer/codeformer.py
@@ -1,43 +1,71 @@
+"""
+This file is used for deploying hugging face demo:
+https://huggingface.co/spaces/sczhou/CodeFormer
+"""
+
+#import sys
+#sys.path.append('CodeFormer')
 import os
 import cv2
-#import argparse
-import glob
 import torch
+import torch.nn.functional as F
 import gradio as gr
+
 from torchvision.transforms.functional import normalize
+
 from basicsr.utils import imwrite, img2tensor, tensor2img
 from basicsr.utils.download_util import load_file_from_url
-from basicsr.utils.misc import gpu_is_available, get_device
 from facelib.utils.face_restoration_helper import FaceRestoreHelper
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from basicsr.utils.realesrgan_utils import RealESRGANer
 from facelib.utils.misc import is_gray
 
 from basicsr.utils.registry import ARCH_REGISTRY
 
-def codeformer_gui():
-    with.ge.Row():
-        codeformer_enabled = gr.Checkbox(label="Enabled", value=False)
-        codeformer_background = gr.Checkbox(value=True, label="Background_Enhance"),
-        codeformer_face_upsample = gr.Checkbox(value=True, label="Face_Upsample"),
-        codeformfer_fidelity = gr.Slider(0, 1, value=0.5, step=0.01, label='Codeformer_Fidelity (0 for better quality, 1 for better identity)')
-    return codeformer_enabled,codeformer_background,codeformer_face_upsample,codeformfer_fidelity
-def check_ckpts():
-    pretrain_model_url = {
-        'codeformer': 'https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/codeformer.pth',
-        'detection': 'https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/detection_Resnet50_Final.pth',
-        'parsing': 'https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/parsing_parsenet.pth',
-        'realesrgan': 'https://huggingface.co/shaitanzx/FooocusExtend/resolve/main/RealESRGAN_x2plus.pth'
-    }
-    # download weights
-    if not os.path.exists('extentions/CodeFormer/weights/CodeFormer/codeformer.pth'):
-        load_file_from_url(url=pretrain_model_url['codeformer'], model_dir='extentions/CodeFormer/weights/CodeFormer', progress=True, file_name=None)
-    if not os.path.exists('extentions/CodeFormer/weights/facelib/detection_Resnet50_Final.pth'):
-        load_file_from_url(url=pretrain_model_url['detection'], model_dir='extentions/CodeFormer/weights/facelib', progress=True, file_name=None)
-    if not os.path.exists('extentions/CodeFormer/weights/facelib/parsing_parsenet.pth'):
-        load_file_from_url(url=pretrain_model_url['parsing'], model_dir='extentions/CodeFormer/weights/facelib', progress=True, file_name=None)
-    if not os.path.exists('extentions/CodeFormer/weights/realesrgan/RealESRGAN_x2plus.pth'):
-        load_file_from_url(url=pretrain_model_url['realesrgan'], model_dir='extentions/CodeFormer/weights/realesrgan', progress=True, file_name=None)
-    
-    
+"""
+os.system("pip freeze")
+
+pretrain_model_url = {
+    'codeformer': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth',
+    'detection': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/detection_Resnet50_Final.pth',
+    'parsing': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/parsing_parsenet.pth',
+    'realesrgan': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/RealESRGAN_x2plus.pth'
+}
+# download weights
+if not os.path.exists('CodeFormer/weights/CodeFormer/codeformer.pth'):
+    load_file_from_url(url=pretrain_model_url['codeformer'], model_dir='CodeFormer/weights/CodeFormer', progress=True, file_name=None)
+if not os.path.exists('CodeFormer/weights/facelib/detection_Resnet50_Final.pth'):
+    load_file_from_url(url=pretrain_model_url['detection'], model_dir='CodeFormer/weights/facelib', progress=True, file_name=None)
+if not os.path.exists('CodeFormer/weights/facelib/parsing_parsenet.pth'):
+    load_file_from_url(url=pretrain_model_url['parsing'], model_dir='CodeFormer/weights/facelib', progress=True, file_name=None)
+if not os.path.exists('CodeFormer/weights/realesrgan/RealESRGAN_x2plus.pth'):
+    load_file_from_url(url=pretrain_model_url['realesrgan'], model_dir='CodeFormer/weights/realesrgan', progress=True, file_name=None)
+
+# download images
+torch.hub.download_url_to_file(
+    'https://replicate.com/api/models/sczhou/codeformer/files/fa3fe3d1-76b0-4ca8-ac0d-0a925cb0ff54/06.png',
+    '01.png')
+torch.hub.download_url_to_file(
+    'https://replicate.com/api/models/sczhou/codeformer/files/a1daba8e-af14-4b00-86a4-69cec9619b53/04.jpg',
+    '02.jpg')
+torch.hub.download_url_to_file(
+    'https://replicate.com/api/models/sczhou/codeformer/files/542d64f9-1712-4de7-85f7-3863009a7c3d/03.jpg',
+    '03.jpg')
+torch.hub.download_url_to_file(
+    'https://replicate.com/api/models/sczhou/codeformer/files/a11098b0-a18a-4c02-a19a-9a7045d68426/010.jpg',
+    '04.jpg')
+torch.hub.download_url_to_file(
+    'https://replicate.com/api/models/sczhou/codeformer/files/7cf19c2c-e0cf-4712-9af8-cf5bdbb8d0ee/012.jpg',
+    '05.jpg')
+torch.hub.download_url_to_file(
+    'https://raw.githubusercontent.com/sczhou/CodeFormer/master/inputs/cropped_faces/0729.png',
+    '06.png')
+
+def imread(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
 # set enhancer with RealESRGAN
 def set_realesrgan():
     half = True if torch.cuda.is_available() else False
@@ -51,7 +79,7 @@ def set_realesrgan():
     )
     upsampler = RealESRGANer(
         scale=2,
-        model_path="extentions/CodeFormer/weights/realesrgan/RealESRGAN_x2plus.pth",
+        model_path="CodeFormer/weights/realesrgan/RealESRGAN_x2plus.pth",
         model=model,
         tile=400,
         tile_pad=40,
@@ -60,19 +88,41 @@ def set_realesrgan():
     )
     return upsampler
 
+upsampler = set_realesrgan()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+codeformer_net = ARCH_REGISTRY.get("CodeFormer")(
+    dim_embd=512,
+    codebook_size=1024,
+    n_head=8,
+    n_layers=9,
+    connect_list=["32", "64", "128", "256"],
+).to(device)
+ckpt_path = "CodeFormer/weights/CodeFormer/codeformer.pth"
+checkpoint = torch.load(ckpt_path)["params_ema"]
+codeformer_net.load_state_dict(checkpoint)
+codeformer_net.eval()
 
-def face_restoration(img, background_enhance, face_upsample, upscale, codeformer_fidelity, upsampler, codeformer_net, device):
+os.makedirs('output', exist_ok=True)
+
+def inference(image, face_align, background_enhance, face_upsample, upscale, codeformer_fidelity):
     """Run a single prediction on the model"""
     try: # global try
         # take the default setting for the demo
-        has_aligned = False
         only_center_face = False
         draw_box = False
         detection_model = "retinaface_resnet50"
 
+        print('Inp:', image, background_enhance, face_upsample, upscale, codeformer_fidelity)
+        face_align = face_align if face_align is not None else True
         background_enhance = background_enhance if background_enhance is not None else True
         face_upsample = face_upsample if face_upsample is not None else True
         upscale = upscale if (upscale is not None and upscale > 0) else 2
+
+        has_aligned = not face_align
+        upscale = 1 if has_aligned else upscale
+
+        img = cv2.imread(str(image), cv2.IMREAD_COLOR)
+        print('\timage size:', img.shape)
 
         upscale = int(upscale) # convert type to int
         if upscale > 4: # avoid memory exceeded due to too large upscale
@@ -91,6 +141,7 @@ def face_restoration(img, background_enhance, face_upsample, upscale, codeformer
             det_model=detection_model,
             save_ext="png",
             use_parse=True,
+            device=device,
         )
         bg_upsampler = upsampler if background_enhance else None
         face_upsampler = upsampler if face_upsample else None
@@ -99,6 +150,8 @@ def face_restoration(img, background_enhance, face_upsample, upscale, codeformer
             # the input faces are already cropped and aligned
             img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_LINEAR)
             face_helper.is_gray = is_gray(img, threshold=5)
+            if face_helper.is_gray:
+                print('\tgrayscale input: True')
             face_helper.cropped_faces = [img]
         else:
             face_helper.read_image(img)
@@ -106,15 +159,14 @@ def face_restoration(img, background_enhance, face_upsample, upscale, codeformer
             num_det_faces = face_helper.get_face_landmarks_5(
             only_center_face=only_center_face, resize=640, eye_dist_threshold=5
             )
+            print(f'\tdetect {num_det_faces} faces')
             # align and warp each face
             face_helper.align_warp_face()
 
         # face restoration for each cropped face
         for idx, cropped_face in enumerate(face_helper.cropped_faces):
             # prepare data
-            cropped_face_t = img2tensor(
-                cropped_face / 255.0, bgr2rgb=True, float32=True
-            )
+            cropped_face_t = img2tensor(cropped_face / 255., bgr2rgb=True, float32=True)
             normalize(cropped_face_t, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
             cropped_face_t = cropped_face_t.unsqueeze(0).to(device)
 
@@ -128,12 +180,10 @@ def face_restoration(img, background_enhance, face_upsample, upscale, codeformer
                 torch.cuda.empty_cache()
             except RuntimeError as error:
                 print(f"Failed inference for CodeFormer: {error}")
-                restored_face = tensor2img(
-                    cropped_face_t, rgb2bgr=True, min_max=(-1, 1)
-                )
+                restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
 
             restored_face = restored_face.astype("uint8")
-            face_helper.add_restored_face(restored_face)
+            face_helper.add_restored_face(restored_face, cropped_face)
 
         # paste_back
         if not has_aligned:
@@ -155,37 +205,33 @@ def face_restoration(img, background_enhance, face_upsample, upscale, codeformer
                 restored_img = face_helper.paste_faces_to_input_image(
                     upsample_img=bg_img, draw_box=draw_box
                 )
+        else:
+            restored_img = restored_face
+
+        # save restored img
+        save_path = f'output/out.png'
+        imwrite(restored_img, str(save_path))
 
         restored_img = cv2.cvtColor(restored_img, cv2.COLOR_BGR2RGB)
         return restored_img
     except Exception as error:
         print('Global exception', error)
         return None, None
-def codeformer_process(source_img,codeformer_background,codeformer_face_upsample,codeformfer_fidelity):
-    check_ckpts()
-    upsampler = set_realesrgan()
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
-    print(f"{device}")
-    codeformer_net = ARCH_REGISTRY.get("CodeFormer")(dim_embd=512,
-                                                        codebook_size=1024,
-                                                        n_head=8,
-                                                        n_layers=9,
-                                                        connect_list=["32", "64", "128", "256"],
-                                                      ).to(device)
-    ckpt_path = "extentions/CodeFormer/weights/CodeFormer/codeformer.pth"
-    checkpoint = torch.load(ckpt_path)["params_ema"]
-    codeformer_net.load_state_dict(checkpoint)
-    codeformer_net.eval()     
-    result_image = cv2.cvtColor(np.array(source_img), cv2.COLOR_RGB2BGR)
-    result_image = face_restoration(result_image, 
-                                      codeformer_background, 
-                                      codeformer_face_upsample, 
-                                      1, 
-                                      codeformfer_fidelity,
-                                      upsampler,
-                                      codeformer_net,
-                                      device)
 
-      swapped_images.append(result_image)
-  
-  return swapped_images
+
+
+"""
+def codeformer_gui():
+    with gr.Row():
+        with gr.Column():
+    inswapper_enabled = gr.Checkbox(label="Enabled", value=False)
+    codeformer_preface=gr.Checkbox(value=True, label="Pre_Face_Align"),
+    inswapper_background_enhance=gr.Checkbox(label="Background Enchanced", value=True),
+    inswapper_face_upsample=gr.Checkbox(label="Face Upsample", value=True)
+    inswapper_upscale = gr.Slider(label='Upscale', minimum=1.0, maximum=4.0, step=1.0, value=1,interactive=True)
+    inswapper_fidelity =gr.Slider(label='Codeformer_Fidelity', minimum=0, maximum=1, value=0.5, step=0.01, info='0 for better quality, 1 for better identity (default=0.5)')
+        with gr.Column():
+    codeformer_input=gr.Image(type="filepath", label="Input"),
+    codeformer_output=gr.Image(type="numpy", label="Output")
+    
+    
